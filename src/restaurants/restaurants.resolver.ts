5f96685fd1be946598e1bd48a2e95f68b1ade6ea
smilejakdu <ash982416@gmail.com>
2021-12-19 04:42:16 +0900

    objectType

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,10 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver()
+export class RestaurantResolver {
+	@Query(() => Restaurant)
+	myRestaurant() {
+		return true;
+	}
+}
