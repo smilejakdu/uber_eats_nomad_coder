786bb9c24ca9ed2563382119937cb796654dc918
작두 <ash982416@gmail.com>
2021-12-19 20:46:08 +0900

    graphql-api-session: validating argsTypes

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,16 +1,22 @@
-import { Field, InputType } from '@nestjs/graphql';
+import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
-@InputType()
+@ArgsType()
 export class CreateRestaurantDto {
-	@Field(type => String)
+	@Field(() => String)
+	@IsString()
+	@Length(5, 10)
 	name: string;
 
-	@Field(type => Boolean)
+	@Field(() => Boolean)
+	@IsBoolean()
 	isVegan: boolean;
 
 	@Field(type => String)
+	@IsString()
 	address: string;
 
-	@Field(type => String)
+	@Field(() => String)
+	@IsString()
 	ownersName: string;
 }
