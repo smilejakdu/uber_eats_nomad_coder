4f17d0eedc361d17309a57726352f4f49724e8fb
smilejakdu <ash982416@gmail.com>
2022-01-09 00:18:16 +0900

    3.8 update restaurant part two

--- src/restaurants/restaurant.resolver.ts
+++ src/restaurants/restaurant.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dto/update-restaurant.dto';
 import { RestaurantEntity } from './entities/restaurant.entity';
 import { RestaurantService } from './restaurant.service';
 
@@ -12,8 +13,20 @@ export class RestaurantResolver {
 	}
 	@Mutation(returns => Boolean)
 	async createRestaurant(@Args('input') createRestaurantDto: CreateRestaurantDto): Promise<boolean> {
+		console.log(createRestaurantDto);
 		try {
 			await this.restaurantService.createRestaurant(createRestaurantDto);
+			return true;
+		} catch (e) {
+			console.log(e);
+			return false;
+		}
+	}
+
+	@Mutation(returns => Boolean)
+	async updateRestaurant(@Args('input') updateRestaurantDto: UpdateRestaurantDto): Promise<boolean> {
+		try {
+			await this.restaurantService.updateRestaurant(updateRestaurantDto);
 			return true;
 		} catch (e) {
 			console.log(e);
--- src/restaurants/restaurant.service.ts
+++ src/restaurants/restaurant.service.ts
@@ -2,21 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dto/update-restaurant.dto';
 import { RestaurantEntity } from './entities/restaurant.entity';
 
 @Injectable()
 export class RestaurantService {
 	constructor(
 		@InjectRepository(RestaurantEntity)
-		private readonly restaurantRepository: Repository<RestaurantEntity>,
+		private readonly restaurants: Repository<RestaurantEntity>,
 	) {}
-
-	async getAll(): Promise<RestaurantEntity[]> {
-		return this.restaurantRepository.find();
+	getAll(): Promise<RestaurantEntity[]> {
+		return this.restaurants.find();
 	}
-
-	async createRestaurant(createRestaurantDto: CreateRestaurantDto): Promise<RestaurantEntity> {
-		const newRestaurant = this.restaurantRepository.create(createRestaurantDto);
-		return await this.restaurantRepository.save(newRestaurant);
+	createRestaurant(createRestaurantDto: CreateRestaurantDto): Promise<RestaurantEntity> {
+		const newRestaurant = this.restaurants.create(createRestaurantDto);
+		return this.restaurants.save(newRestaurant);
+	}
+	updateRestaurant({ id, data }: UpdateRestaurantDto) {
+		return this.restaurants.update(id, { ...data });
 	}
 }
